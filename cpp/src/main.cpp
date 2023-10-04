cea8753898e4be0b7a798d5a7c4d34f20342e52f
Daniel Vieira <danielvie@gmail.com>
2023-10-04 13:29:37 +0200

    return to stack

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     
-    std::unique_ptr<Model> model = std::make_unique<Model>();
+    Model model;
 
     std::ofstream logFile("states.log");
     if (!logFile.is_open()) {
@@ -11,22 +11,25 @@ int main() {
         return -1;
     }
 
+    std::cout << "initial delta x1: " << model.m_states.x1 << std::endl;
+    std::cout << "initial delta x2: " << model.m_states.x2 << std::endl;
+
     // Logging the state values
-    while(model->m_t <= model->m_t_end) {
-        logFile << model->m_t << ","
-            << model->m_states.x1 << ","
-            << model->m_states.x2 << ","
-            << model->m_states.v1 << ","
-            << model->m_states.v2 << std::endl;
+    while(model.m_t <= model.m_t_end) {
+        logFile << model.m_t << ","
+            << model.m_states.x1 << ","
+            << model.m_states.x2 << ","
+            << model.m_states.v1 << ","
+            << model.m_states.v2 << std::endl;
 
         // make a step of simulation
-        model->step();
+        model.step();
     }
 
     logFile.close();
 
-    std::cout << "final delta x1: " << model->m_states.x1 << std::endl;
-    std::cout << "final delta x2: " << model->m_states.x2 << std::endl;
+    std::cout << "final delta x1: " << model.m_states.x1 << std::endl;
+    std::cout << "final delta x2: " << model.m_states.x2 << std::endl;
 
     return 0;
 }
